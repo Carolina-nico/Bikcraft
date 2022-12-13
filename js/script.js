3460f09af3657814299a38c2891a20879a4d0a44
Carolina-nico <cs.uxuiti@gmail.com>
2022-12-13 11:49:02 -0300

    Ativando as perguntas e respostas. E galeria de imagens.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Ativar links do Menu // header-menu
+// Ativar links do Menu // header-menu ----------
 const links = document.querySelectorAll("header nav li a");
 
 // console.log("javascript" .includes("a")); // // Uma forma de verificar se é true ou false
@@ -13,8 +13,7 @@ function ativarLink(link) {
 }
 links.forEach(ativarLink);
 
-// Ativar Itens do Orçamento
-
+// Ativar Itens do Orçamento -----------
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
@@ -29,3 +28,39 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto);
 
+// Perguntas Frequentes ------------
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    console.log(ativa);
+
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas -----------
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    galeriaContainer.prepend(img); //Prepend: funciona da seguinte forma, tenho uma img grande e duas pequenas. Seleciono a pequena e ela vai mudar de lugar e ir para a grande e vise versa.
+    console.log(img);
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
